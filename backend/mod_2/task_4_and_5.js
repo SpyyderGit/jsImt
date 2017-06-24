207f1db33a9a38a23d30ac030b03248604678636
SpYYdeR <spyyders.git@gmail.com>
2017-06-24 21:33:56 +0300

    fifth task is ready

--- backend/mod_2/task_4_and_5.js
+++ backend/mod_2/task_4_and_5.js
@@ -16,7 +16,6 @@ var emiter = new EventEmitter();
 var port = 3000;
 
 function currency() {
-
     var curr = '[{"ccy":"EUR","base_ccy":"UAH","buy":"29.39461","sale":"29.54840"},\
     {"ccy":"RUR","base_ccy":"UAH","buy":"0.46309","sale":"0.46311"},\
     {"ccy":"USD","base_ccy":"UAH","buy":"26.20541","sale":"26.24658"},\
